731ae1ec1e1eb162cb871b9768118f5de3450afa
Uday Kumar Gorantla <129682715+Nani2139@users.noreply.github.com>
2025-09-21 09:49:33 +0530

    Update PostFeed.js

--- src/components/organisms/PostFeed/PostFeed.js
+++ src/components/organisms/PostFeed/PostFeed.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import PostCard from "../../molecules/PostCard/PostCard";
 import Button from "../../atoms/Button/Button";
